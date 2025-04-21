b6d9268db75d0cbc38d80a06a2442999d6d77397
FionaGachuuri <fionairuka@gmail.com>
2025-04-22 00:37:23 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  // Call the uploadPhoto function to upload the user's photo then chain all
-  // promises together and handle with one error statement
+  // Call the uploadPhoto function to upload the user's photo then
+  // chain all promises together and handle with an error statement
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((userResp) => {
+    .then(([photo, user]) => {
       // If the user was successfully created, log the body, firstName, and lastName to the console
-      console.log(`${userResp[0].body} ${userResp[1].firstName} ${userResp[1].lastName}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       // In the event of an error, log Signup system offline to the console
